909a0d7d0bf96a513f7597b8840aa75b4220aef9
AbdullahAzza1 <abdullahazza141@gmail.com>
2024-01-19 19:22:49 +0100

    step4

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,12 @@
+<header>
+    <a href="">characters</a>
+    <a href="">comics</a>
+    <a href="">movies</a>
+    <a href="">tv</a>
+    <a href="">games</a>
+    <a href="">collectibles</a>
+    <a href="">videos</a>
+    <a href="">fans</a>
+    <a href="">news</a>
+    <a href="">shop</a>
+</header>
